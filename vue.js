2b080d39f9de77ae04c4fcf27044fc07c965aedc
Luis <qiqe-123@hotmail.com>
2021-06-29 21:42:03 -0500

    Se hizo el pedo pa

--- vue.js
+++ vue.js
@@ -1,11 +1,25 @@
-var app = new Vue({
-el: '.vue',
-data: {
-    mensaje: "Aprende vue.js fácilmente",
-},
+  var app = new Vue({
+    el:'#app',
+    data:{
+        titulo : 'Lista de tareas',
+        tareas: [
+            {
+                texto: 'Aprender vue.js',
+                terminada : false
+            },
+            {
+                texto: 'Aprender angular 2 ',
+                terminada : false
+            },
+            {
+                texto: 'Aprender iconic 2',
+                terminada : false
+            },
+        ],
+        nuevaTarea = ''
 
-methods: {
-    mostrarMensajes: function(){
-        return this.mensaje;
+    },
+    methods:{
+        
     }
-}})
+ })
