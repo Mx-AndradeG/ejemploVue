0d178b02739c571e4a50a06f80e599ef0ff4db73
Luis <qiqe-123@hotmail.com>
2021-06-30 09:17:53 -0500

    ESte es un commit de ejemplo

--- vue.js
+++ vue.js
@@ -1,25 +1,67 @@
-  var app = new Vue({
+Vue.component('titulo',{
+    template: '<h2>{{titulo}}</h2>',
+    data:function(){
+        return{ titulo: '- Lista de tareas -'}
+    }
+})
+
+    Vue.component('nueva-tarea',{
+    template: `
+        <div class="input-group">
+        <input type="text" 
+            placeholder="escribe una tarea" 
+            v-model='nuevaTarea'
+            class="form-control"
+            v-on:keypress.enter = "agregarTarea">
+         <span class="input-group-btn">
+            <button type="button" 
+            v-on:click="agregarTarea()" 
+            class="btn btn-primary">Agregar tarea</button>
+         </span>
+        </div>`,
+
+        data: function(){
+            return data;
+        },
+
+        methods:{
+            agregarTarea: function(){
+                var texto = this.nuevaTarea.trim();
+                if(texto){
+                    this.tareas.push({
+                        texto: texto,
+                        terminada: false
+                    })
+                }
+            }
+        }
+    })
+
+
+
+new Vue({
     el:'#app',
     data:{
         titulo : 'Lista de tareas',
         tareas: [
             {
                 texto: 'Aprender vue.js',
-                terminada : false
+                terminada: false
             },
             {
-                texto: 'Aprender angular 2 ',
-                terminada : false
+                texto: 'Aprende Angular 2',
+                terminada :false
             },
             {
-                texto: 'Aprender iconic 2',
-                terminada : false
-            },
+                texto: 'Aprender Iconic 2',
+                terminada: false
+            }
         ],
-        nuevaTarea = ''
-
+        nuevaTarea: ''
     },
     methods:{
-        
+        borrar: function(indice){
+            this.tareas.splice(indice,1);
+        }
     }
  })
